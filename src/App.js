61f70dd3b7d7287e5e3034b194cec0516809122e
Dayaldonadkar <dayalpython@gmail.com>
2023-11-01 23:55:32 +0530

    three sections created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Stories from "./Component/Stories";
+import Search from "./Component/Search";
+import Pagination from "./Component/Pagination";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Search />
+      <Pagination />
+      <Stories />
+    </div>
+  );
 };
 
 export default App;
