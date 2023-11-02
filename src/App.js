23ea5c7133f64f1904bd62cc2fab88fe6d7c6522
Dayaldonadkar <dayalpython@gmail.com>
2023-11-02 23:12:09 +0530

    d

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from "react";
 import Stories from "./Component/Stories";
 import Search from "./Component/Search";
 import Pagination from "./Component/Pagination";
+import { useGlobalContext } from "./Context/Context";
 
 const App = () => {
+  const data = useGlobalContext();
   return (
     <div>
+      <h1>{data}</h1>
       <Search />
       <Pagination />
       <Stories />
