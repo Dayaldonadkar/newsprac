798a7e12c2ee70b8b9fcd5a5e9a0b2b7a346e0f7
Dayaldonadkar <dayalpython@gmail.com>
2023-11-03 23:39:28 +0530

    reducer created

--- src/Component/Stories.js
+++ src/Component/Stories.js
@@ -1,22 +1,6 @@
 import React, { useEffect } from "react";
 
 const Stories = () => {
-  const API = "https://hn.algolia.com/api/v1/search?query=html";
-
-  const fetchApiData = async (url) => {
-    try {
-      const res = await fetch(url);
-      const data = await res.json();
-      console.log("fetched", data.hits);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchApiData(API);
-  }, []);
-
   return (
     <div>
       <h2>Welcome to news</h2>
--- src/Context/Context.js
+++ src/Context/Context.js
@@ -1,8 +1,34 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
+import { Reducer } from "react";
 
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
+  const API = "https://hn.algolia.com/api/v1/search?query=html";
+
+  const initialState = {
+    query: "html",
+    nbPages: 0,
+    page: 0,
+    hits: [],
+    isLoading: true,
+  };
+
+  const [state, dispatch] = useReducer(Reducer, initialState);
+
+  const fetchApiData = async (url) => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log("fetched", data.hits);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchApiData(API);
+  }, []);
   return <AppContext.Provider value={"dayal"}>{children}</AppContext.Provider>;
 };
 
